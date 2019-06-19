d98d40920dbcfd7a6c4f92d9ca67b097e9707b67
skenone <51911493+skenone@users.noreply.github.com>
2019-06-19 10:44:57 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -15,8 +15,4 @@ foreach ($messages as $message)
 	{
 		$bot->sendTextMessage($recipientId, "Attachment received");
 	}
-	elseif($message->text=='Link')
-	{
-		$bot->sendTextMessage($recipientId, "http:\\www.google.it\");
-	}
 }
