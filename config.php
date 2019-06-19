35f1ee1c0c095f538555a47254d7ab479b980711
skenone <51911493+skenone@users.noreply.github.com>
2019-06-19 10:29:16 +0200

    Update config.php
    Valorizzazione FACEBOOK_PAGE_ID

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 define("FACEBOOK_PAGE_ACCESS_TOKEN", "Insert here");
-define("FACEBOOK_PAGE_ID", "Insert here");
+define("FACEBOOK_PAGE_ID", "389957504968558");
 define("FACEBOOK_VALIDATION_TOKEN", "789456123");
