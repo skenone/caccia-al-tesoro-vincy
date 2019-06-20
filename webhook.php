f6f8a4dfaf3cff0a327e30f684400c4391a2d98d
skenone <51911493+skenone@users.noreply.github.com>
2019-06-20 15:58:09 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,7 +1,20 @@
+
 <?php
 require_once 'config.php';
 require_once 'FacebookBot.php';
+
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
+$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Greetings! The humans who invented me programmed me to tell you about...");
+if($updated)
+{
+	echo "Welcome Message updated succesfully!";
+}
+else 
+{
+	echo "Error during Welcome Message update";
+}
+/*
+
 $bot->run();
 $messages = $bot->getReceivedMessages();
 $bot->sendTyping($recipientId);
@@ -33,3 +46,4 @@ foreach ($messages as $message)
 		$bot->sendTextMessage($recipientId, $message->text);
 	}
 }
+*/
