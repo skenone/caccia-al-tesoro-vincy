27c7d0c0a7661fe68e82b4bc5fd45bfe1ba1a322
skenone <51911493+skenone@users.noreply.github.com>
2019-06-20 16:25:07 +0200

    Update setWelcomeMessage.php

--- setWelcomeMessage.php
+++ setWelcomeMessage.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
-$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Greetings! The humans who invented me programmed me to tell you about...");
+$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Ciao Umano!");
 if($updated)
 {
 	echo "Welcome Message updated succesfully!";
