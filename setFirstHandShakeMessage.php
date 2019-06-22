fba1216f19b4521ac52508a5edb2bf796c0b304e
skenone <51911493+skenone@users.noreply.github.com>
2019-06-22 09:55:57 +0200

    Update setFirstHandShakeMessage.php

--- setFirstHandShakeMessage.php
+++ setFirstHandShakeMessage.php
@@ -3,6 +3,7 @@ require_once 'config.php';
 require_once 'FacebookSetHandShake.php';
 $bot = new HandShake(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $updated = $bot->setFirstHandShakeMessage(FACEBOOK_PAGE_ID);
+echo $updateed;
 if($updated)
 {
 	echo "FirstHandShake Message updated succesfully!";
