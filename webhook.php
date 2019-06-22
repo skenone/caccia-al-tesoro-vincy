0ae05f14c68a929298303861e078d6754c13fb49
skenone <51911493+skenone@users.noreply.github.com>
2019-06-22 16:57:51 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once 'config.php';
 require_once 'FacebookBot.php';
@@ -39,4 +38,3 @@ foreach ($messages as $message)
 		$bot->sendTextMessage($recipientId, $message->text);
 	}
 }
-
