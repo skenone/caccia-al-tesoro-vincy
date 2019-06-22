ea390e7442d7130789c17fb3d4bec280be2c84ec
skenone <51911493+skenone@users.noreply.github.com>
2019-06-22 14:40:04 +0200

    Update FacebookSetHandShake.php

--- FacebookSetHandShake.php
+++ FacebookSetHandShake.php
@@ -40,7 +40,7 @@ class HandShake
       
       "payload"=>"First_Hand_shake"
     ]];
-        echo implode(",",$answer);
+        echo son_encode($answer);
         $parameters = $answer;    
         $response = self::executePost($url, $request, true);
         if ($response) {
