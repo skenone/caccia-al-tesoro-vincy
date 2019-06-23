da08f8afca057160f5f5057b41722fa96b5e5222
skenone <51911493+skenone@users.noreply.github.com>
2019-06-23 10:07:22 +0200

    Update GetPersona.php

--- GetPersona.php
+++ GetPersona.php
@@ -5,6 +5,7 @@ require_once 'Persona.php';
 echo " Inizio";
 
 $persona = new Persona(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
+echo "persona Inizializzata";
 $persona->run();
 
 echo $persona->get_Persona($persona->senderId);
