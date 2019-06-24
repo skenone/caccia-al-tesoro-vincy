0f16a1e406bf3f8c253b8b48a1fef3fe700fe01b
skenone <51911493+skenone@users.noreply.github.com>
2019-06-24 11:03:26 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,32 +3,32 @@ require_once 'config.php';
 require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $bot->run();
-$messages = strtoupper($bot->getReceivedMessages());
+$messages = $bot->getReceivedMessages();
 /*$bot->sendTextMessage($messages[0]->senderId, json_encode($messages[0]));*/
 foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
-	if($message->text=='LINK')
+	if(strtoupper($message->text)=='LINK')
 	{
 		$bot->sendLinkMessage($recipientId, "http://www.google.it/");
 	}
-	elseif($message->text=='ID')
+	elseif(strtoupper($message->text)=='ID')
 	{
 		$bot->sendTextMessage($recipientId, $message->senderId);
 	}
-	elseif($message->text=='TYPING')
+	elseif(strtoupper($message->text)=='TYPING')
 	{
 		$bot->sendTyping($recipientId);
 	}
-	elseif($message->text=='REGOLAMENTO')
+	elseif(strtoupper($message->text)=='REGOLAMENTO')
 	{
 		$bot->sendRegolamento($recipientId);
 	}
-	elseif($message->text=='IMAGE')
+	elseif(strtoupper($message->text)=='IMAGE')
 	{
 		$bot->sendImageMessage($recipientId, "http://www.like-agency.it/media/k2/items/cache/d6086de322f98f66cc694f32ea284557_L.jpg");
 	}
-	elseif($message->text=='IMAGE2')
+	elseif(strtoupper($message->text)=='IMAGE2')
 	{
 		$bot->sendImageMessage($recipientId, "http://www.like-agency.it/media/k2/items/cache/d6086de322f98f66cc694f32ea284557_L.jpg");
 		$bot->sendImageMessage($recipientId, "https://upload.wikimedia.org/wikipedia/it/5/51/Immagine_13.png");
