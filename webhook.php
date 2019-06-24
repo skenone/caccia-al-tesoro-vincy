a5004b4a2eac0d937a5004a53ab9d7613a223112
skenone <51911493+skenone@users.noreply.github.com>
2019-06-24 11:00:39 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,12 +3,12 @@ require_once 'config.php';
 require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $bot->run();
-$messages = $bot->getReceivedMessages();
+$messages = strtoupper($bot->getReceivedMessages());
 /*$bot->sendTextMessage($messages[0]->senderId, json_encode($messages[0]));*/
 foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
-	if($message->text=='Link')
+	if($message->text=='LINK')
 	{
 		$bot->sendLinkMessage($recipientId, "http://www.google.it/");
 	}
@@ -16,19 +16,19 @@ foreach ($messages as $message)
 	{
 		$bot->sendTextMessage($recipientId, $message->senderId);
 	}
-	elseif($message->text=='Typing')
+	elseif($message->text=='TYPING')
 	{
 		$bot->sendTyping($recipientId);
 	}
-	elseif($message->text=='Regolamento')
+	elseif($message->text=='REGOLAMENTO')
 	{
 		$bot->sendRegolamento($recipientId);
 	}
-	elseif($message->text=='Image')
+	elseif($message->text=='IMAGE')
 	{
 		$bot->sendImageMessage($recipientId, "http://www.like-agency.it/media/k2/items/cache/d6086de322f98f66cc694f32ea284557_L.jpg");
 	}
-	elseif($message->text=='Image2')
+	elseif($message->text=='IMAGE2')
 	{
 		$bot->sendImageMessage($recipientId, "http://www.like-agency.it/media/k2/items/cache/d6086de322f98f66cc694f32ea284557_L.jpg");
 		$bot->sendImageMessage($recipientId, "https://upload.wikimedia.org/wikipedia/it/5/51/Immagine_13.png");
