728d20fcbb6faee779fb2faad4764842c0550cff
skenone <51911493+skenone@users.noreply.github.com>
2019-06-25 20:21:56 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -21,7 +21,7 @@ foreach ($messages as $message)
 	elseif(strtoupper($message->text)=='ISCRIVI')
 	{
 			
-			if($InterDB->isSubscriber==0){
+			if($InterDB->isSubscriber($recipientId)==0){
 			$bot->sendTextMessage($recipientId,"$homepage");
 			$bot->sendTextMessage($recipientId,"[🤖] :Per iscrivere la tua squadra devi scrivermi in un unico messaggio il nome della squadra seguito da # e  i componenti (NomeTeam#ListaComponentiTeam). 
 			esempio : ");
