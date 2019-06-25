a8b1196589a004d170b243999137a92b788676b3
skenone <51911493+skenone@users.noreply.github.com>
2019-06-25 19:48:18 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -18,7 +18,10 @@ foreach ($messages as $message)
 	}
 	elseif(strtoupper($message->text)=='ISCRIVI')
 	{
-		
+			$url = "http://skenone.altervista.org/CacciaAlTesoro/controllaIscrizione.php?ID_CAPITANO=%s";
+        		$url = sprintf($url,$recipientId);
+        		$homepage = file_get_contents($url);
+			$bot->sendTextMessage($recipientId,"$homepage");
 			$bot->sendTextMessage($recipientId,"[🤖] :Per iscrivere la tua squadra devi scrivermi in un unico messaggio il nome della squadra seguito da # e  i componenti (NomeTeam#ListaComponentiTeam). 
 			esempio : ");
 			$bot->sendTextMessage($recipientId,"SquadraDisney#Topolino,Pluto,Pippo,Paperino");
