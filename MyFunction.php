68fe9ccc4ef6def00ef9ee89abee2cb69faf422e
skenone <51911493+skenone@users.noreply.github.com>
2019-06-25 19:31:29 +0200

    Update MyFunction.php

--- MyFunction.php
+++ MyFunction.php
@@ -2,13 +2,9 @@
 
     public function controllaIscrizione($recipientId)
     {
-        $url = "http://skenone.altervista.org/CacciaAlTesoro/controllaIscrizione.php?ID_CAPITANO=%s&";
-        $url = sprintf($url, $this->getPageAccessToken());
-        $recipient = new \stdClass();
-        $recipient->id = $recipientId;
-        $sender_action="typing_on";
-        $parameters = ['recipient' => $recipient, 'sender_action' => $sender_action];
-        $response = self::executePost($url, $parameters, true);
+        $url = "http://skenone.altervista.org/CacciaAlTesoro/controllaIscrizione.php?ID_CAPITANO=%s";
+        $url = sprintf($url, $this->$recipientId());
+                $response = self::executeGet($url, true);
         if ($response) {
             $responseObject = json_decode($response);
             return is_object($responseObject) && isset($responseObject->recipient_id) && isset($responseObject->message_id);
@@ -22,22 +18,10 @@
         $content = file_get_contents("php://input");
         return json_decode($content, false, 512, JSON_BIGINT_AS_STRING);
     }
-    private static function executePost($url, $parameters, $json = false)
+    private static function executeGet($url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        if ($json) {
-            $data = json_encode($parameters);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data)));
-        } else {
-            curl_setopt($ch, CURLOPT_POST, count($parameters));
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-        }
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $response = curl_exec($ch);
         curl_close($ch);
         return $response;
