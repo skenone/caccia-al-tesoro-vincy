8d6064648f721125d83534682adf4ef68e830646
skenone <51911493+skenone@users.noreply.github.com>
2019-06-28 17:31:00 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,8 +11,8 @@ foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
 	if(strtoupper($message->text)=='CODE'){
-		/*$aiuti=json_decode($InterDB->getAiuti("CIAO"));
-		$bot->sendTextMessage($recipientId,$aiuti->a1);
+		$aiuti=json_decode($InterDB->getAiuti("CIAO"));
+		/*$bot->sendTextMessage($recipientId,$aiuti->a1);
 		sleep(60);
 		$bot->sendTextMessage($recipientId,$aiuti->a2);
 		sleep(60);
