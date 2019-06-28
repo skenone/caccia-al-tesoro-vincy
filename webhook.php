9c1a8b7ea08d071121c68abc4a339430a651acab
skenone <51911493+skenone@users.noreply.github.com>
2019-06-28 16:57:16 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -14,6 +14,15 @@ foreach ($messages as $message)
 		$aiuti=json_decode($InterDB->getAiuti("CIAO"));
 		//$bot->sendTextMessage($recipientId,$aiuti['a1']);
 		$bot->sendTextMessage($recipientId,$aiuti->a1);
+		sleep(60);
+		$bot->sendTextMessage($recipientId,$aiuti->a2);
+		sleep(60);
+		$bot->sendTextMessage($recipientId,$aiuti->a3);
+		sleep(60);
+		$bot->sendTextMessage($recipientId,$aiuti->a5);
+		sleep(60);
+		$bot->sendTextMessage($recipientId,$aiuti->a6);
+			
 	}
 	elseif(strtoupper($message->text)=='LINK')
 	{
