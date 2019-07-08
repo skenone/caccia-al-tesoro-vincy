ff8116d2d7fde43b2a0b408d4efe0c5c2187ac7d
skenone <51911493+skenone@users.noreply.github.com>
2019-07-08 12:26:28 +0200

    Update FacebookBot.php

--- FacebookBot.php
+++ FacebookBot.php
@@ -188,7 +188,7 @@ class FacebookBot
 												"buttons"=>[
 															[
 																"type"=>"web_url",
-																"url"=>"http://www.google.it/",
+																"url"=>"https://www.facebook.com/notes/veni-vidi-vincy/regolamento-la-notte-del-tesoro-2019-26-luglio-2019/2542545305777294/",
 																"title"=>"Regolamento"
 															]            
 															]
